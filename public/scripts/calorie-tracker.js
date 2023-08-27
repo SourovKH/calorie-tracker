f8fcabafa5dc12e7ae35fc541b5d5d2054c8927e
Sourov <SourovKH@users.noreply.github.com>
2023-08-27 20:24:56 +0530

    add feature to submit exercises duration or count

--- public/scripts/calorie-tracker.js
+++ public/scripts/calorie-tracker.js
@@ -28,8 +28,51 @@ const handleTargetSetter = () => {
   };
 };
 
+const getExerciseDetails = () => {
+  const exerciseElements = document.querySelectorAll(".exercise-measurement");
+
+  const exerciseDetails = [...exerciseElements].map((exerciseElement) => {
+    const exerciseName = exerciseElement.name;
+    const exerciseMeasurement = exerciseElement.value;
+
+    return [exerciseName, +exerciseMeasurement];
+  });
+
+  return Object.fromEntries(exerciseDetails);
+};
+
+const updateCalorieBoard = (remainingCalorie) => {
+  const achieveElement = document.querySelector("#achieved");
+  achieveElement.innerText = `${remainingCalorie} cal`;
+};
+
+const submitExercises = () => {
+  const exerciseDetails = getExerciseDetails();
+  const request = {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({ ...exerciseDetails }),
+  };
+
+  fetch("/calorie-tracker/exercises", request)
+    .then((res) => res.json())
+    .then((body) => updateCalorieBoard(body.remainingCalorie));
+};
+
+const handleExercises = () => {
+  const form = document.querySelector("#exercise-measurements");
+
+  form.onsubmit = (event) => {
+    event.preventDefault();
+    submitExercises();
+  };
+};
+
 const main = () => {
   handleTargetSetter();
+  handleExercises();
 };
 
 window.onload = main;
